8c71a594dda5113b06e23a842406432c2e59918a
blushyvil <blushyvil@gmail.com>
2026-09-08 22:13:41 +0700

    ping pong work

---
+++ index.js
@@ -0,0 +1,51 @@
+const makeWASocket = require('baileys').default
+const { useMultiFileAuthState, DisconnectReason } = require('baileys')
+const { Boom } = require('@hapi/boom')
+const pino = require('pino')
+const qrcode = require('qrcode-terminal')
+
+async function startbot() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+
+    const sock = makeWASocket({
+        auth: state,
+        logger: pino({ level: 'silent' })
+    })
+
+    sock.ev.on('creds.update', saveCreds)
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect, qr  } = update
+
+        if (qr) {
+            console.log('QR RECEIVED! scan this:')
+            qrcode.generate(qr, { small: true })
+        }
+
+        if (connection === 'close') {
+            const shouldReconnect = new Boom(lastDisconnect?.error)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('connection lost! reconnecting...', shouldReconnect)
+            if (shouldReconnect) startbot()
+        } else if (connection === 'open') {
+    console.log('mundane connected!')
+}
+    })
+
+    sock.ev.on('messages.upsert', async (m) => {
+
+        const { messages, type } = m
+        if (type !== 'notify') return 
+
+        const msg = messages[0]
+        if (!msg.message || msg.key.fromMe) return
+
+        const text = msg.message.conversation || msg.message.extendedTextMessage?.text || ''
+        const from = msg.key.remoteJid
+
+        if (text.toLowerCase() === 'ping') {
+            await sock.sendMessage(from, {text: 'pong!'})
+        }
+    })
+}
+
+startbot()
